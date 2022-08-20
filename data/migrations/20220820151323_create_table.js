708f78908d78c56e28c688c29661d92df3d89fcb
Jeremiah Thomas <jeremiah.b.thomas12@gmail.com>
2022-08-20 11:31:38 -0400

    create_tables migration is working

---
+++ data/migrations/20220820151323_create_table.js
@@ -0,0 +1,32 @@
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("projects", (tbl) => {
+      tbl.increments("project_id");
+      tbl.varchar("project_name", 100).notNullable();
+      tbl.varchar("project_description");
+      tbl.boolean("projects_completed").defaultTo(0);
+    })
+    .createTable("resources", (tbl) => {
+      tbl.increments("resource_id");
+      tbl.varchar("resource_name", 100).notNullable().unique();
+      tbl.varchar("resource_description", 200);
+    })
+    .createTable("tasks", (tbl) => {
+      tbl.increments("task_id");
+      tbl.varchar("task_description", 200).notNullable();
+      tbl.varchar("task_notes", 300);
+      tbl.boolean("task_completed").defaultTo(0);
+      tbl
+        .integer("project_id")
+        .references("project_id")
+        .inTable("projects")
+        .notNullable();
+    });
+};
+
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists("tasks")
+    .dropTableIfExists("resources")
+    .dropTableIfExists("projects");
+};
